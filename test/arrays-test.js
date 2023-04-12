448d67a1a015b596ae417e8d89c9adf49f9a55bc
Biswajit Sen <BiswajitSen@users.noreply.github.com>
2023-04-12 13:08:46 +0530

    fixed exports and require failures

--- test/arrays-test.js
+++ test/arrays-test.js
@@ -1,6 +1,7 @@
-
+const obj = require('../src/arrays.js');
+const test = require('../lib/testing.js');
 const testSelectEvenNumbers = function() {
-	test.assertListsEqual([2, 4, 6], selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
+  test.assertListsEqual([2, 4, 6], obj.selectEvenNumbers([1,2,3,4,5,6,7]), "Select even numbers");
 }
 
 testSelectEvenNumbers();
